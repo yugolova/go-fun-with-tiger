6a8dce992fe3cb853474326f5a5df7452b53d600
yulia golova <yu.golova@gmail.com>
2015-12-28 19:48:53 +0300

    test1

--- go_fun_with_tiger/spiders/picture_spider.py
+++ go_fun_with_tiger/spiders/picture_spider.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 EARLIEST_YEAR = 1985
 OUT_OF_RANGE_ERROR = "requested date is out of range"
 
-test = "master3"
+test = "master4"
 
 class DayByDayPictureSpider(scrapy.Spider):
     name = "calvin_and_hobbes_pics"
